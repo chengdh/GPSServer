157c2b49522479d0bc83ac728b78939ef3c022a5
chengdh <cheng.donghui@gmail.com>
2013-04-03 15:48:02 +0800

    modify accept_login

--- TCPServer/Protocol/YaxunProtocol.py
+++ TCPServer/Protocol/YaxunProtocol.py
@@ -288,7 +288,7 @@ class YaxunProtocol(protocol.Protocol):
       utc_seconds = int(time.mktime(datetime.datetime.now().timetuple()))
       #data = "\x7e\xfe\x13\x40\x06"+convert(utc_seconds)+"ok\x0d"
       #data = "\x7e\xfe\x13\x40\x0b" + hex_utc_timestamp + "welcome\x0d"
-      data = "\x7e\xfe\x13\x40\x07\x9c\xca\x5b\x51\x00ok\x0d"
+      data = "\x7e\xfe\x13\x40\x07\x01\x0f\x0f\x01\x00ok\x0d"
       log.msg("accept_login")
       self.transport.write(data)
 
